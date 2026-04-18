00792fc813a903bc82d286754258c79765e4e5a7
Alex Kasko <alex@staticlibs.net>
2026-04-18 18:43:01 +0100

    Pool: support configuring acquire mode
    This PR adds new configuration option:
    
     - `pg_pool_acquire_mode` (`VARCHAR`, default: 'force'): how to acquire
       connections from the pool: 'force' (always connect, ignore pool
       limit), 'wait' (block until available), 'try' (fail immediately if
       unavailable)
    
    This option is only applied to Postgres databases attached after the
    option is set.
    
    To configure it for Postgres database that is already attached, the
    `acquire_mode` named parameter is also added to
    `postgres_configure_pool()` function:
    
    ```sql
    FROM postgres_configure_pool(catalog_name='s', acquire_mode='wait')
    ```
    
    Note, before this option the default pool behaviour was `wait`, now the
    default behaviour is changed to `force`.

--- src/include/storage/postgres_connection_pool.hpp
+++ src/include/storage/postgres_connection_pool.hpp
@@ -24,6 +24,8 @@ class PostgresConnectionPool;
 
 using PostgresPoolConnection = dbconnector::pool::PooledConnection<PostgresConnection>;
 
+enum class PostgresPoolAcquireMode { FORCE, WAIT, TRY };
+
 class PostgresConnectionPool : public dbconnector::pool::ConnectionPool<PostgresConnection> {
 public:
 	PostgresConnectionPool(PostgresCatalog &postgres_catalog, ClientContext &context);
@@ -36,9 +38,14 @@ public:
 
 	std::string GetHealthCheckQuery();
 	void SetHealthCheckQuery(const std::string &query);
+	PostgresPoolAcquireMode GetAcquireMode();
+	void SetAcquireMode(PostgresPoolAcquireMode mode);
 
 	static idx_t DefaultPoolSize();
 	static std::string DefaultHealthCheckQuery();
+	static PostgresPoolAcquireMode AcquireModeFromString(const std::string &mode_str);
+	static std::string AcquireModeToString(PostgresPoolAcquireMode mode);
+	static void ValidatePoolAcquireMode(ClientContext &context, SetScope scope, Value &parameter);
 
 protected:
 	std::unique_ptr<PostgresConnection> CreateNewConnection() override;
@@ -50,6 +57,9 @@ private:
 
 	std::mutex config_mutex;
 	std::string health_check_query;
+	PostgresPoolAcquireMode acquire_mode = PostgresPoolAcquireMode::FORCE;
+
+	bool PoolEnabled();
 };
 
 class PostgresConfigurePoolFunction : public TableFunction {
