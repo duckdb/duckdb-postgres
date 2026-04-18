d66838ea938aeb3cf61f9f4d17e677e1f68d6e98
Alex Kasko <alex@staticlibs.net>
2026-04-18 01:44:57 +0100

    Add Windows test runs to CI

--- create-postgres-tables.sh
+++ create-postgres-tables.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+set -x
 
 DUCKDB_PATH=duckdb
 if test -f build/release/duckdb; then
@@ -10,21 +11,27 @@ elif test -f build/debug/duckdb; then
   DUCKDB_PATH=build/debug/duckdb
 fi
 
+if [ -z "${PGSCANNERTMP_ABS_DIR_PREFIX}" ]; then
+  ABS_DIR_PREFIX="/tmp"
+else
+  ABS_DIR_PREFIX="${PGSCANNERTMP_ABS_DIR_PREFIX//\\//}"
+fi
+
 echo "
 CREATE SCHEMA tpch; 
 CREATE SCHEMA tpcds;
 CALL dbgen(sf=0.01, schema='tpch');
 CALL dsdgen(sf=0.01, schema='tpcds');
-EXPORT DATABASE '/tmp/postgresscannertmp';
+EXPORT DATABASE '${ABS_DIR_PREFIX}/postgresscannertmp';
 " | \
 $DUCKDB_PATH
 
 dropdb --if-exists postgresscanner
 createdb postgresscanner
 
-psql -d postgresscanner < /tmp/postgresscannertmp/schema.sql
-psql -d postgresscanner < /tmp/postgresscannertmp/load.sql
-rm -rf /tmp/postgresscannertmp
+psql -d postgresscanner < ${ABS_DIR_PREFIX}/postgresscannertmp/schema.sql
+psql -d postgresscanner < ${ABS_DIR_PREFIX}/postgresscannertmp/load.sql
+rm -rf ${ABS_DIR_PREFIX}/postgresscannertmp
 
 psql -d postgresscanner < test/all_pg_types.sql
 psql -d postgresscanner < test/decimals.sql
