4e800f378cec4c0f3853e075cefb5b3c7e2eceb8
Alex Kasko <alex@staticlibs.net>
2026-07-19 19:02:30 +0100

    Support insertion with plain INSERTs
    The extension uses `COPY ... FROM STDIN (FORMAT BINARY)` when performing
    inserts into Postgres tables. This can cause problems with some
    Postgres-wire-compatible databases that do not support `COPY`.
    
    This PR adds support for performing plain batch `INSERTs` instead of the
    `COPY`. Existing option `pg_use_text_protocol` is used to enable this
    behaviour.
    
    Currently only basic column types are supported, support for composite
    types (`ARRAY`, `LIST`, `STRUCT`) may be added in future.
    
    Testing: existing type tests are updated to also run with text protocol
    enabled.

--- src/include/storage/postgres_insert.hpp
+++ src/include/storage/postgres_insert.hpp
@@ -15,17 +15,18 @@
 #include "duckdb/planner/parsed_data/bound_create_table_info.hpp"
 #include "duckdb/planner/logical_operator.hpp"
 #include "duckdb/common/index_vector.hpp"
+#include "storage/postgres_catalog.hpp"
 
 namespace duckdb {
 
 class PostgresInsert : public PhysicalOperator {
 public:
 	//! INSERT INTO
-	PostgresInsert(PhysicalPlan &physical_plan, LogicalOperator &op, TableCatalogEntry &table,
-	               physical_index_vector_t<idx_t> column_index_map);
+	PostgresInsert(PhysicalPlan &physical_plan, PostgresCatalog &pg_catalog, ClientContext &context,
+	               LogicalOperator &op, TableCatalogEntry &table, physical_index_vector_t<idx_t> column_index_map);
 	//! CREATE TABLE AS
-	PostgresInsert(PhysicalPlan &physical_plan, LogicalOperator &op, SchemaCatalogEntry &schema,
-	               unique_ptr<BoundCreateTableInfo> info);
+	PostgresInsert(PhysicalPlan &physical_plan, PostgresCatalog &pg_catalog, ClientContext &context,
+	               LogicalOperator &op, SchemaCatalogEntry &schema, unique_ptr<BoundCreateTableInfo> info);
 
 	//! The table to insert into
 	optional_ptr<TableCatalogEntry> table;
@@ -37,6 +38,8 @@ public:
 	physical_index_vector_t<idx_t> column_index_map;
 	//! Whether or not we can keep the copy alive during Sink calls
 	bool keep_copy_alive = true;
+	//! Whether to use plain INSERTs instead of COPY
+	bool use_plain_inserts = false;
 
 public:
 	// Source interface
@@ -49,10 +52,30 @@ public:
 
 public:
 	// Sink interface
-	unique_ptr<GlobalSinkState> GetGlobalSinkState(ClientContext &context) const override;
-	SinkResultType Sink(ExecutionContext &context, DataChunk &chunk, OperatorSinkInput &input) const override;
+	unique_ptr<GlobalSinkState> GetGlobalSinkState(ClientContext &context) const override {
+		if (use_plain_inserts) {
+			return GetGlobalSinkStatePlain(context);
+		} else {
+			return GetGlobalSinkStateCopy(context);
+		}
+	}
+
+	SinkResultType Sink(ExecutionContext &context, DataChunk &chunk, OperatorSinkInput &input) const override {
+		if (use_plain_inserts) {
+			return SinkPlain(context, chunk, input);
+		} else {
+			return SinkCopy(context, chunk, input);
+		}
+	}
+
 	SinkFinalizeType Finalize(Pipeline &pipeline, Event &event, ClientContext &context,
-	                          OperatorSinkFinalizeInput &input) const override;
+	                          OperatorSinkFinalizeInput &input) const override {
+		if (use_plain_inserts) {
+			return FinalizePlain(pipeline, event, context, input);
+		} else {
+			return FinalizeCopy(pipeline, event, context, input);
+		}
+	}
 
 	bool IsSink() const override {
 		return true;
@@ -64,6 +87,17 @@ public:
 
 	string GetName() const override;
 	InsertionOrderPreservingMap<string> ParamsToString() const override;
+
+private:
+	static bool UsePlainInserts(PostgresCatalog &pg_catalog, ClientContext &context);
+	unique_ptr<GlobalSinkState> GetGlobalSinkStateCopy(ClientContext &context) const;
+	unique_ptr<GlobalSinkState> GetGlobalSinkStatePlain(ClientContext &context) const;
+	SinkResultType SinkCopy(ExecutionContext &context, DataChunk &chunk, OperatorSinkInput &input) const;
+	SinkResultType SinkPlain(ExecutionContext &context, DataChunk &chunk, OperatorSinkInput &input) const;
+	SinkFinalizeType FinalizeCopy(Pipeline &pipeline, Event &event, ClientContext &context,
+	                              OperatorSinkFinalizeInput &input) const;
+	SinkFinalizeType FinalizePlain(Pipeline &pipeline, Event &event, ClientContext &context,
+	                               OperatorSinkFinalizeInput &input) const;
 };
 
 } // namespace duckdb
